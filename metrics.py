de9654f9d8e484bf4baa5a4f244db3cad79887ca
Sankalp Gilda <sankalp.gilda@gmail.com>
2021-01-14 02:44:49 -0500

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+from scipy.stats import norm
+
 EPS = 1e-6
 
 
@@ -65,8 +67,6 @@ def pinaw(yt, yp, confint=0.6827):
     pinaw = np.mean(yp_upper - yp_lower)/iqr
     return pinaw
 
-
-from scipy.stats import norm
 def cdf_normdist(y, loc=0, scale=1):
     y = np.asarray(y).reshape(-1,)
     loc = np.asarray(loc).reshape(-1,)
@@ -74,11 +74,8 @@ def cdf_normdist(y, loc=0, scale=1):
     u = []
     for y_sample, loc_sample, scale_sample in zip(y, loc, scale):
         rv = norm(loc=loc_sample, scale=scale_sample)
-        #print('line1')
         x = np.linspace(rv.ppf(q=0.01), rv.ppf(q=0.99), 1000)
-        #print('line2')
         u_sample = rv.cdf(y_sample)
-        #print(u_sample)
         u.append(u_sample)
     u = np.asarray(u)
     return u
@@ -90,7 +87,6 @@ def interval_sharpness(yt, yp, confint=0.6827):
     yp_lower = np.asarray(yp_lower).reshape(-1,)
     yp_upper = np.asarray(yp_upper).reshape(-1,)
     yt = cdf_normdist(yt, loc=yp_mean, scale=0.5*(yp_upper-yp_lower))
-    #print(yt)
     alpha = 1-confint
     yp_lower = np.ones_like(yp_lower)*(0.5-confint/2)
     yp_upper = np.ones_like(yp_upper)*(0.5+confint/2)
@@ -99,20 +95,5 @@ def interval_sharpness(yt, yp, confint=0.6827):
     intsharp = np.nanmean(np.greater_equal(yt, yp_upper)*(-2*alpha*delta_alpha - 4*(yt - yp_upper)) + np.greater_equal(yp_lower, yt)*(-2*alpha*delta_alpha - 4*(yp_lower - yt)) + -2*alpha*delta_alpha)
     return intsharp
 
-'''
-def interval_sharpness(yt, yp, confint=0.6827):
-    yt = np.asarray(yt).flatten()
-    yp_mean, yp_lower, yp_upper = yp
-    yp_mean = np.asarray(yp_mean).reshape(-1,)
-    yp_lower = np.asarray(yp_lower).reshape(-1,)
-    yp_upper = np.asarray(yp_upper).reshape(-1,)
-    alpha = 1-confint
-    delta_alpha = yp_upper - yp_lower
-    intsharp = np.greater_equal(yt, yp_upper)*(2*alpha*delta_alpha + 4*(yt - yp_upper)) + np.less_equal(yt, yp_lower)*(2*alpha*delta_alpha + 4*(yp_lower - yt)) + np.equal(np.less_equal(yt, yp_upper), np.greater_equal(yt, yp_lower))*(2*alpha*delta_alpha)
-    iqr = (np.quantile(intsharp, 0.95) - np.quantile(intsharp, 0.05)) + EPS  
-    intsharp_norm = np.mean((intsharp - np.min(intsharp))/(np.max(intsharp)-np.min(intsharp)))
-    intsharp_norm = np.mean(intsharp/iqr)
-    return intsharp_norm
-'''
 
 
