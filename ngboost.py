9cbc442cce8097ffe54bc0324d75a4684d9187df
Sankalp Gilda <sankalp.gilda@gmail.com>
2021-01-14 03:15:00 -0500

    Add files via upload

---
+++ ngboost.py
@@ -0,0 +1,460 @@
+#Sankalp. Changes made on 07/20/2020. and copied to new fork on 09/14/2020:
+# added a function to add gaussian noise to samples
+
+import numpy as np
+import scipy as sp
+
+from ngboost.scores import LogScore
+from ngboost.distns import Normal, k_categorical
+from ngboost.manifold import manifold
+from ngboost.learners import default_tree_learner, default_linear_learner
+
+from sklearn.utils import check_random_state, check_X_y, check_array
+from sklearn.base import clone
+from sklearn.tree import DecisionTreeRegressor
+
+# import pdb
+
+
+class NGBoost(object):
+    """
+    Constructor for all NGBoost models.
+
+    This class implements the methods that are common to all NGBoost models. Unless you are implementing a new kind of regression (e.g. interval-censored, etc.), you should probably use one of NGBRegressor, NGBClassifier, or NGBSurvival.
+
+    Parameters:
+        Dist              : assumed distributional form of Y|X=x. A distribution from ngboost.distns, e.g. Normal
+        Score             : rule to compare probabilistic predictions P̂ to the observed data y. A score from ngboost.scores, e.g. LogScore
+        Base              : base learner to use in the boosting algorithm. Any instantiated sklearn regressor, e.g. DecisionTreeRegressor()
+        natural_gradient  : logical flag indicating whether the natural gradient should be used
+        n_estimators      : the number of boosting iterations to fit
+        learning_rate     : the learning rate
+        minibatch_frac    : the percent subsample of rows to use in each boosting iteration
+        verbose           : flag indicating whether output should be printed during fitting
+        verbose_eval      : increment (in boosting iterations) at which output should be printed
+        tol               : numerical tolerance to be used in optimization
+        random_state      : seed for reproducibility. See https://stackoverflow.com/questions/28064634/random-state-pseudo-random-number-in-scikit-learn
+    Output:
+        An NGBRegressor object that can be fit.
+    """
+
+    def __init__(
+        self,
+        Dist=Normal,
+        Score=LogScore,
+        Base=default_tree_learner,
+        natural_gradient=True,
+        n_estimators=500,
+        learning_rate=0.01,
+        minibatch_frac=1.0,
+        col_sample=1.0,
+        verbose=True,
+        verbose_eval=100,
+        tol=1e-4,
+        random_state=None,
+    ):
+        self.Dist = Dist
+        self.Score = Score
+        self.Base = Base
+        self.Manifold = manifold(Score, Dist)
+        self.natural_gradient = natural_gradient
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.minibatch_frac = minibatch_frac
+        self.col_sample = col_sample
+        self.verbose = verbose
+        self.verbose_eval = verbose_eval
+        self.init_params = None
+        self.n_features = None
+        self.base_models = []
+        self.scalings = []
+        self.col_idxs = []
+        self.tol = tol
+        self.random_state = check_random_state(random_state)
+        self.best_val_loss_itr = None
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        # Remove the unpicklable entries.
+        del state["Manifold"]
+        if self.Dist.__name__ == "Categorical":
+            del state["Dist"]
+            state["K"] = self.Dist.n_params + 1
+        return state
+
+    def __setstate__(self, state_dict):
+        if "K" in state_dict.keys():
+            state_dict["Dist"] = k_categorical(state_dict["K"])
+        state_dict["Manifold"] = manifold(state_dict["Score"], state_dict["Dist"])
+        self.__dict__ = state_dict
+
+    def fit_init_params_to_marginal(self, Y, sample_weight=None, iters=1000):
+        self.init_params = self.Manifold.fit(
+            Y
+        )  # would be best to put sample weights here too
+        return
+
+    def pred_param(self, X, max_iter=None):
+        m, n = X.shape
+        params = np.ones((m, self.Manifold.n_params)) * self.init_params
+        for i, (models, s, col_idx) in enumerate(
+            zip(self.base_models, self.scalings, self.col_idxs)
+        ):
+            if max_iter and i == max_iter:
+                break
+            resids = np.array([model.predict(X[:, col_idx]) for model in models]).T
+            params -= self.learning_rate * resids * s
+        return params
+
+    def sample(self, X, Y, sample_weight, params):
+        idxs = np.arange(len(Y))
+        col_idx = np.arange(X.shape[1])
+
+        if self.minibatch_frac != 1.0:
+            sample_size = int(self.minibatch_frac * len(Y))
+            idxs = self.random_state.choice(
+                np.arange(len(Y)), sample_size, replace=False
+            )
+
+        if self.col_sample != 1.0:
+            col_size = int(self.col_sample * X.shape[1])
+            col_idx = self.random_state.choice(
+                np.arange(X.shape[1]), col_size, replace=False
+            )
+
+        weight_batch = None if sample_weight is None else sample_weight[idxs]
+
+        return (
+            idxs,
+            col_idx,
+            X[idxs, :][:, col_idx],
+            Y[idxs],
+            weight_batch,
+            params[idxs, :],
+        )
+
+    def fit_base(self, X, grads, sample_weight=None):
+        models = [
+            clone(self.Base).fit(X, g, sample_weight=sample_weight) for g in grads.T
+        ]
+        fitted = np.array([m.predict(X) for m in models]).T
+        self.base_models.append(models)
+        return fitted
+
+    def line_search(self, resids, start, Y, sample_weight=None, scale_init=1):
+        S = self.Score
+        D_init = self.Manifold(start.T)
+        loss_init = D_init.total_score(Y, sample_weight)
+        scale = scale_init
+
+        # first scale up
+        while True:
+            scaled_resids = resids * scale
+            D = self.Manifold((start - scaled_resids).T)
+            loss = D.total_score(Y, sample_weight)
+            norm = np.mean(np.linalg.norm(scaled_resids, axis=1))
+            if not np.isfinite(loss) or loss > loss_init or scale > 256:
+                break
+            scale = scale * 2
+
+        # then scale down
+        while True:
+            scaled_resids = resids * scale
+            D = self.Manifold((start - scaled_resids).T)
+            loss = D.total_score(Y, sample_weight)
+            norm = np.mean(np.linalg.norm(scaled_resids, axis=1))
+            if norm < self.tol:
+                break
+            if np.isfinite(loss) and loss < loss_init:
+                break
+            scale = scale * 0.5
+        self.scalings.append(scale)
+        return scale
+
+    def noise_maker(self, X, X_noise):
+        # assuming X is already log10(1+flux_measured)-scaled, 
+        # and X_noise is an array of percentages.
+        X_noise = np.asarray(X_noise).reshape(1,-1)
+        noise = X_noise * np.random.default_rng().normal(size=(X.shape))
+        X_noisy_nonlog = (10**X - 1)*(1+noise)
+        # the clip in the following operation is to simulate non-detection. skipping for now.
+        #X_noisy = np.log10(1 + X_noisy_nonlog)
+        X_noisy = np.log10(1 + np.clip(a=X_noisy_nonlog, a_min=0., a_max=None))
+        # due to 35 filters. 36th variable, in chaining mode, is redshift.
+        q = np.where(X_noisy_nonlog[:,:35]==0)[0]
+        if len(q)>0:
+            pass
+            ###print('noise value = ',np.max(X_noise))
+            #print('sample index/indices with noisy flux values = 0: ',set(q))
+        return X_noisy
+
+    def fit(
+        self,
+        X,
+        Y,
+        X_val=None,
+        Y_val=None,
+        sample_weight=None,
+        val_sample_weight=None,
+        train_loss_monitor=None,
+        val_loss_monitor=None,
+        early_stopping_rounds=None,
+        X_noise = None,
+    ):
+        """
+        Fits an NGBoost model to the data
+
+        Parameters:
+            X                       : DataFrame object or List or numpy array of predictors (n x p) in Numeric format
+            Y                       : DataFrame object or List or numpy array of outcomes (n) in Numeric format. Should be floats for regression and integers from 0 to K-1 for K-class classification
+            X_val                   : DataFrame object or List or numpy array of validation-set predictors in Numeric format, if any
+            Y_val                   : DataFrame object or List or numpy array of validation-set outcomes in Numeric format, if any
+            sample_weight           : how much to weigh each example in the training set. numpy array of size (n) (defaults to 1)
+            val_sample_weight       : how much to weigh each example in the validation set. (defaults to 1)
+            train_loss_monitor      : a custom score or set of scores to track on the training set during training. Defaults to the score defined in the NGBoost constructor
+            val_loss_monitor        : a custom score or set of scores to track on the validation set during training. Defaults to the score defined in the NGBoost constructor
+            early_stopping_rounds   : the number of consecutive boosting iterations during which the loss has to increase before the algorithm stops early
+
+        Output:
+            A fit NGBRegressor object
+        """
+
+        if Y is None:
+            raise ValueError("y cannot be None")
+
+        X, Y = check_X_y(X, Y, y_numeric=True)
+
+        self.n_features = X.shape[1]
+
+        loss_list = []
+        self.fit_init_params_to_marginal(Y)
+
+        params = self.pred_param(X)
+        if X_val is not None and Y_val is not None:
+            X_val, Y_val = check_X_y(X_val, Y_val, y_numeric=True)
+            val_params = self.pred_param(X_val)
+            val_loss_list = []
+            best_val_loss = np.inf
+
+        if not train_loss_monitor:
+            train_loss_monitor = lambda D, Y, W: D.total_score(Y, sample_weight=W)
+
+        if not val_loss_monitor:
+            val_loss_monitor = lambda D, Y: D.total_score(
+                Y, sample_weight=val_sample_weight
+            )
+
+        for itr in range(self.n_estimators):
+            _, col_idx, X_batch, Y_batch, weight_batch, P_batch = self.sample(
+                X, Y, sample_weight, params
+            )
+
+            ### adding noise. Sankalp, 07/20/2020, and then 08/28/2020, and then 09/14/20###
+            # add 0% noise by default
+            if X_noise is None:
+                X_noise = 0.0# * np.ones((1,X_batch.shape[1])) 
+
+            X_batch = self.noise_maker(X_batch, X_noise)
+            ### END OF ADDING NOISE ###
+            
+            self.col_idxs.append(col_idx)
+
+            D = self.Manifold(P_batch.T)
+
+            loss_list += [train_loss_monitor(D, Y_batch, weight_batch)]
+            loss = loss_list[-1]
+            grads = D.grad(Y_batch, natural=self.natural_gradient)
+
+            proj_grad = self.fit_base(X_batch, grads, weight_batch)
+            scale = self.line_search(proj_grad, P_batch, Y_batch, weight_batch)
+
+            # pdb.set_trace()
+            params -= (
+                self.learning_rate
+                * scale
+                * np.array([m.predict(X[:, col_idx]) for m in self.base_models[-1]]).T
+            )
+
+            val_loss = 0
+            if X_val is not None and Y_val is not None:
+                val_params -= (
+                    self.learning_rate
+                    * scale
+                    * np.array(
+                        [m.predict(X_val[:, col_idx]) for m in self.base_models[-1]]
+                    ).T
+                )
+                val_loss = val_loss_monitor(self.Manifold(val_params.T), Y_val)
+                val_loss_list += [val_loss]
+                if val_loss < best_val_loss:
+                    best_val_loss, self.best_val_loss_itr = val_loss, itr
+                if (
+                    early_stopping_rounds is not None
+                    and len(val_loss_list) > early_stopping_rounds
+                    and best_val_loss
+                    < np.min(np.array(val_loss_list[-early_stopping_rounds:]))
+                ):
+                    if self.verbose:
+                        print(f"== Early stopping achieved.")
+                        print(
+                            f"== Best iteration / VAL{self.best_val_loss_itr} (val_loss={best_val_loss:.4f})"
+                        )
+                    break
+
+            if (
+                self.verbose
+                and int(self.verbose_eval) > 0
+                and itr % int(self.verbose_eval) == 0
+            ):
+                grad_norm = np.linalg.norm(grads, axis=1).mean() * scale
+                print(
+                    f"[iter {itr}] loss={loss:.4f} val_loss={val_loss:.4f} scale={scale:.4f} "
+                    f"norm={grad_norm:.4f}"
+                )
+
+            if np.linalg.norm(proj_grad, axis=1).mean() < self.tol:
+                if self.verbose:
+                    print(f"== Quitting at iteration / GRAD {itr}")
+                break
+
+        self.evals_result = {}
+        metric = self.Score.__name__.upper()
+        self.evals_result["train"] = {metric: loss_list}
+        if X_val is not None and Y_val is not None:
+            self.evals_result["val"] = {metric: val_loss_list}
+
+        return self
+
+    def score(self, X, Y):  # for sklearn
+        return self.Manifold(self.pred_dist(X)._params).total_score(Y)
+
+    def pred_dist(self, X, max_iter=None):
+        """
+        Predict the conditional distribution of Y at the points X=x
+
+        Parameters:
+            X        : DataFrame object or List or numpy array of predictors (n x p) in Numeric Format
+            max_iter : get the prediction at the specified number of boosting iterations
+
+        Output:
+            A NGBoost distribution object
+        """
+
+        X = check_array(X)
+
+        if (
+            max_iter is not None
+        ):  # get prediction at a particular iteration if asked for
+            dist = self.staged_pred_dist(X, max_iter=max_iter)[-1]
+        else:
+            params = np.asarray(self.pred_param(X, max_iter))
+            dist = self.Dist(params.T)
+        return dist
+
+    def staged_pred_dist(self, X, max_iter=None):
+        """
+        Predict the conditional distribution of Y at the points X=x at multiple boosting iterations
+
+        Parameters:
+            X        : numpy array of predictors (n x p)
+            max_iter : largest number of boosting iterations to get the prediction for
+
+        Output:
+            A list of NGBoost distribution objects, one per boosting stage up to max_iter
+        """
+        predictions = []
+        m, n = X.shape
+        params = np.ones((m, self.Dist.n_params)) * self.init_params
+        for i, (models, s, col_idx) in enumerate(
+            zip(self.base_models, self.scalings, self.col_idxs)
+        ):
+            resids = np.array([model.predict(X[:, col_idx]) for model in models]).T
+            params -= self.learning_rate * resids * s
+            dists = self.Dist(
+                np.copy(params.T)
+            )  # if the params aren't copied, param changes with stages carry over to dists
+            predictions.append(dists)
+            if max_iter and i == max_iter:
+                break
+        return predictions
+
+    def predict(self, X, max_iter=None):
+        """
+        Point prediction of Y at the points X=x
+
+        Parameters:
+            X        : DataFrame object or List or numpy array of predictors (n x p) in Numeric Format
+            max_iter : get the prediction at the specified number of boosting iterations
+
+        Output:
+            Numpy array of the estimates of Y
+        """
+
+        X = check_array(X)
+
+        return self.pred_dist(X, max_iter=max_iter).predict()
+
+    def staged_predict(self, X, max_iter=None):
+        """
+        Point prediction of Y at the points X=x at multiple boosting iterations
+
+        Parameters:
+            X        : numpy array of predictors (n x p)
+            max_iter : largest number of boosting iterations to get the prediction for
+
+        Output:
+            A list of numpy arrays of the estimates of Y, one per boosting stage up to max_iter
+        """
+        return [dist.predict() for dist in self.staged_pred_dist(X, max_iter=max_iter)]
+
+    @property
+    def feature_importances_(self):
+        """
+        Return the feature importances for all parameters in the distribution
+            (the higher, the more important the feature).
+
+        Returns
+        -------
+        feature_importances_ : array, shape = [n_params, n_features]
+            The summation along second axis of this array is an array of ones,
+            unless all trees are single node trees consisting of only the root
+            node, in which case it will be an array of zeros.
+        """
+        # Check whether the model is fitted
+        if not self.base_models:
+            return None
+        # Check whether the base model is DecisionTreeRegressor
+        if not isinstance(self.base_models[0][0], DecisionTreeRegressor):
+            return None
+        # Reshape the base_models
+        params_trees = zip(*self.base_models)
+
+        # Get the feature_importances_ for all the params and all the trees
+        all_params_importances = [
+            [
+                self._get_feature_importance(tree, tree_index)
+                for tree_index, tree in enumerate(trees)
+            ]
+            for trees in params_trees
+        ]
+
+        if not all_params_importances:
+            return np.zeros(
+                len(self.base_models[0]),
+                self.base_models[0][0].n_features_,
+                dtype=np.float64,
+            )
+
+        # Weighted average of importance by tree scaling factors
+        all_params_importances = np.average(
+            all_params_importances, axis=1, weights=self.scalings
+        )
+        return all_params_importances / np.sum(
+            all_params_importances, axis=1, keepdims=True
+        )
+
+    def _get_feature_importance(self, tree, tree_index):
+        tree_feature_importance = getattr(tree, "feature_importances_")
+        total_feature_importance = np.zeros(self.n_features)
+        total_feature_importance[self.col_idxs[tree_index]] = tree_feature_importance
+        return total_feature_importance
---
+++ normal.py
@@ -0,0 +1,153 @@
+from ngboost.distns import RegressionDistn
+from ngboost.scores import LogScore, CRPScore
+import scipy as sp
+import numpy as np
+from scipy.stats import norm as dist
+
+
+class NormalLogScore(LogScore):
+    def score(self, Y):
+        return -self.dist.logpdf(Y)
+
+    def d_score(self, Y):
+        D = np.zeros((len(Y), 2))
+        D[:, 0] = (self.loc - Y) / self.var
+        # 09/15/2020: changed ** to np.float_power to avoid overflow errors
+        D[:, 1] = 1 - np.float_power(self.loc - Y, 2) / self.var
+        return D
+
+    def metric(self):
+        FI = np.zeros((self.var.shape[0], 2, 2))
+        FI[:, 0, 0] = 1 / self.var
+        FI[:, 1, 1] = 2
+        return FI
+
+
+class NormalCRPScore(CRPScore):
+    def score(self, Y):
+        Z = (Y - self.loc) / self.scale
+        return self.scale * (
+            Z * (2 * sp.stats.norm.cdf(Z) - 1)
+            + 2 * sp.stats.norm.pdf(Z)
+            - 1 / np.sqrt(np.pi)
+        )
+
+    def d_score(self, Y):
+        Z = (Y - self.loc) / self.scale
+        D = np.zeros((len(Y), 2))
+        D[:, 0] = -(2 * sp.stats.norm.cdf(Z) - 1)
+        D[:, 1] = self.score(Y) + (Y - self.loc) * D[:, 0]
+        return D
+
+    def metric(self):
+        I = np.c_[
+            2 * np.ones_like(self.var),
+            np.zeros_like(self.var),
+            np.zeros_like(self.var),
+            self.var,
+        ]
+        I = I.reshape((self.var.shape[0], 2, 2))
+        I = 1 / (2 * np.sqrt(np.pi)) * I
+        return I
+
+
+class Normal(RegressionDistn):
+    """
+    Implements the normal distribution for NGBoost.
+
+    The normal distribution has two parameters, loc and scale, which are the mean and standard deviation, respectively.
+    This distribution has both LogScore and CRPScore implemented for it.
+    """
+
+    n_params = 2
+    scores = [NormalLogScore, NormalCRPScore]
+
+    def __init__(self, params):
+        super().__init__(params)
+        self.loc = params[0]
+        #Sankalp: adding the clipping line on 10/16/2020.
+        q = np.clip(params[1], a_min=-1e1, a_max=1e1)
+        self.scale = np.exp(q)
+        #Sankalp: adding the clipping line on 10/16/2020.
+        self.scale = np.clip(self.scale, a_min=0, a_max=1e1)
+        self.var = np.float_power(self.scale,2)
+        self.dist = dist(loc=self.loc, scale=self.scale)
+
+    def fit(Y):
+        m, s = sp.stats.norm.fit(Y)
+        return np.array([m, np.log(s)])
+
+    def sample(self, m):
+        return np.array([self.rvs() for i in range(m)])
+
+    def __getattr__(
+        self, name
+    ):  # gives us Normal.mean() required for RegressionDist.predict()
+        if name in dir(self.dist):
+            return getattr(self.dist, name)
+        return None
+
+    @property
+    def params(self):
+        return {"loc": self.loc, "scale": self.scale}
+
+
+### Fixed Variance Normal ###
+class NormalFixedVarLogScore(LogScore):
+    def score(self, Y):
+        return -self.dist.logpdf(Y)
+
+    def d_score(self, Y):
+        D = np.zeros((len(Y), 1))
+        D[:, 0] = (self.loc - Y) / self.var
+        return D
+
+    def metric(self):
+        FI = np.zeros((self.var.shape[0], 1, 1))
+        FI[:, 0, 0] = 1 / self.var + 1e-5
+        return FI
+
+
+class NormalFixedVarCRPScore(CRPScore):
+    def score(self, Y):
+        Z = (Y - self.loc) / self.scale
+        return self.scale * (
+            Z * (2 * sp.stats.norm.cdf(Z) - 1)
+            + 2 * sp.stats.norm.pdf(Z)
+            - 1 / np.sqrt(np.pi)
+        )
+
+    def d_score(self, Y):
+        Z = (Y - self.loc) / self.scale
+        D = np.zeros((len(Y), 1))
+        D[:, 0] = -(2 * sp.stats.norm.cdf(Z) - 1)
+        return D
+
+    def metric(self):
+        I = np.c_[2 * np.ones_like(self.var)]
+        I = I.reshape((self.var.shape[0], 1, 1))
+        I = 1 / (2 * np.sqrt(np.pi)) * I
+        return I
+
+
+class NormalFixedVar(Normal):
+    """
+    Implements the normal distribution with variance=1 for NGBoost.
+
+    The fixed-variance normal distribution has one parameters, loc which is the mean.
+    This distribution has both LogScore and CRPScore implemented for it.
+    """
+
+    n_params = 1
+    scores = [NormalFixedVarLogScore, NormalFixedVarCRPScore]
+
+    def __init__(self, params):
+        self.loc = params[0]
+        self.var = np.ones_like(self.loc)
+        self.scale = np.ones_like(self.loc)
+        self.shape = self.loc.shape
+        self.dist = dist(loc=self.loc, scale=self.scale)
+
+    def fit(Y):
+        m, s = sp.stats.norm.fit(Y)
+        return m
