0782ccb64a349f995bf03434d4f1fe724481aaa1
Sankalp Gilda <sankalp.gilda@gmail.com>
2023-06-18 13:46:21 -0400

    test_shap_handler.py passes

--- src/shap_handler.py
+++ src/shap_handler.py
@@ -1,7 +1,3 @@
-
-from numpy import ndarray
-from pydantic import BaseModel, validator
-from typing import Optional, Tuple
 import logging
 from joblib import dump, load
 from sklearn.exceptions import NotFittedError
@@ -45,8 +41,8 @@ class ShapHandler:
     def create(self, fitted_base_estimator: BaseEstimator) -> None:
         """
         Calculate the SHAP Explainer.
-        1. If calculation_mode is True, it creates the explainer. Additionally, if model_handler.file_path is provided, it saves the explainer there.
-        2. If calculation_mode is False, it loads a saved explainer from the file_path.
+        1. If fitting_mode is True, it creates the explainer. Additionally, if model_handler.file_path is provided, it saves the explainer there.
+        2. If fitting_mode is False, it loads a saved explainer from the file_path.
         """
         validate_file_path(self._config.shap_file_path,
                            self._config.fitting_mode)
@@ -62,13 +58,34 @@ class ShapHandler:
         2. If not, create the explainer.
         3. Save the explainer if a file_path is provided.
         """
+        if self._explainer is not None:
+            logger.warning("SHAP Explainer has already been created.")
+            return
+
+        if not hasattr(fitted_base_estimator, "fit"):
+            logger.error(
+                "The provided estimator has no 'fit' method. Cannot calculate SHAP values.")
+            raise ValueError("Invalid estimator: 'fit' method not found.")
+
+        if not hasattr(fitted_base_estimator, "estimators_"):
+            logger.error(
+                "The provided estimator has not been fitted. Cannot calculate SHAP values."
+            )
+            raise ValueError(
+                "Invalid estimator: estimator has not been fitted.")
+
         if self._config.precreated_explainer is not None:
             self.explainer = self._config.precreated_explainer
         else:
             data_train = self._get_shap_data(self._config.X_train)
-            self.explainer = shap.TreeExplainer(
-                fitted_base_estimator, data=data_train, model_output=0, feature_names=self._config.feature_names)
-            logger.info("SHAP explainer created")
+            if not hasattr(fitted_base_estimator, "predict_std") or not hasattr(fitted_base_estimator, "predict_dist"):
+                self.explainer = shap.TreeExplainer(
+                    fitted_base_estimator, data=data_train, feature_names=self._config.feature_names)
+            else:
+                self.explainer = shap.TreeExplainer(
+                    fitted_base_estimator, data=data_train, model_output=0, feature_names=self._config.feature_names)
+
+        logger.info("SHAP explainer created")
 
         self._save_explainer()
 
@@ -76,7 +93,13 @@ class ShapHandler:
         """
         Save the explainer to a file specified by file_path.
         """
-        dump(self.explainer, self._config.shap_file_path)
+        if self._config.shap_file_path is not None:
+            try:
+                dump(self.explainer, self._config.shap_file_path)
+            except (ValueError, IOError) as e:
+                logger.warning(f"Failed to save the explainer: {e}")
+        else:
+            logger.warning("No filename provided. Skipping save.")
 
     def _load_explainer(self) -> None:
         """
@@ -91,111 +114,3 @@ class ShapHandler:
         else:
             data = X
         return data
-
-
-logger = logging.getLogger(__name__)
-
-
-class ModelHandlerConfig(BaseModel):
-    """
-    Configuration class for ModelHandler.
-    """
-    X_train: Optional[ndarray]
-    y_train: Optional[ndarray]
-    X_val: Optional[ndarray]
-    y_val: Optional[ndarray]
-    estimator_file_path: str
-    shap_file_path: str
-    shap_values_path: str
-
-    @validator('X_val', 'y_val', always=True)
-    def check_val(cls, value, values, field):
-        if values.get('X_val') is not None and values.get('y_val') is None:
-            raise ValueError(
-                f"{field.name} is required when X_val is provided")
-        return value
-
-    @validator('X_train', 'y_train', 'X_val', 'y_val', always=True)
-    def check_lengths(cls, value, values, field):
-        if values.get('X_train') is not None and values.get('y_train') is not None:
-            if len(values['X_train']) != len(values['y_train']):
-                raise ValueError("Lengths of X_train and y_train must match")
-        if values.get('X_val') is not None and values.get('y_val') is not None:
-            if len(values['X_val']) != len(values['y_val']):
-                raise ValueError("Lengths of X_val and y_val must match")
-        return value
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
-class ModelHandler:
-    """
-    A model handler for performing various tasks including data transformation,
-    fitting/loading an estimator, and computing prediction bounds and SHAP values.
-    """
-
-    def __init__(self, config: ModelHandlerConfig) -> None:
-        self._config = config
-        self._estimator = None
-        self._shap_handler = None
-        self._shap_values = None
-
-    @property
-    def estimator(self) -> BaseEstimator:
-        if self._estimator is None:
-            try:
-                self._estimator = load(self._config.estimator_file_path)
-            except Exception as e:
-                logger.error(
-                    f"Error occurred while loading the estimator: {e}")
-        return self._estimator
-
-    @estimator.setter
-    def estimator(self, value: BaseEstimator) -> None:
-        self._estimator = value
-        self._save_estimator()
-
-    @property
-    def explainer(self) -> shap.TreeExplainer:
-        if self._shap_handler is None:
-            self._shap_handler = ShapHandler(self._config)
-        return self._shap_handler.explainer
-
-    @explainer.setter
-    def explainer(self, value: shap.TreeExplainer) -> None:
-        if self._shap_handler is None:
-            self._shap_handler = ShapHandler(self._config)
-        self._shap_handler.explainer = value
-
-    @property
-    def shap_values(self) -> ndarray:
-        if self._shap_values is None:
-            self._shap_values = self._shap_handler.load_shap_values(
-                self._config.shap_values_path)
-        return self._shap_values
-
-    def fit(self) -> None:
-        if self._config.X_train is None or self._config.y_train is None:
-            raise ValueError(
-                "X_train or y_train is not set in the configuration.")
-        # Fit the estimator with the training data.
-        self.estimator.fit(self._config.X_train, self._config.y_train)
-
-    def predict(self, X: Optional[ndarray]) -> Optional[ndarray]:
-        return self.estimator.predict(X) if self.estimator is not None else None
-
-    def create_explainer(self) -> None:
-        # Assumes estimator is a Tree-based model.
-        self.explainer = shap.TreeExplainer(self.estimator)
-
-    def calculate_and_save_shap_values(self, X: Optional[ndarray]) -> None:
-        self._shap_values = self.explainer.shap_values(X)
-        self._shap_handler.save_shap_values(
-            self._config.shap_values_path, self._shap_values)
-
-    def _save_estimator(self) -> None:
-        try:
-            dump(self.estimator, self._config.estimator_file_path)
-        except Exception as e:
-            logger.error(f"Error occurred while saving the estimator: {e}")
--- tests/test_shap_handler.py
+++ tests/test_shap_handler.py
@@ -1,84 +1,103 @@
+import numpy as np
+import pytest
+from pathlib import Path
+from sklearn.exceptions import NotFittedError
+from sklearn.ensemble import RandomForestRegressor
+from src.shap_handler import ShapHandler
+from src.model_handler import ModelHandlerConfig
+import shap
+import logging
+from sklearn.base import BaseEstimator
 
-@pytest.mark.parametrize("fit_before_predict", [True, False])
-def test_calculate_shap_values(dummy_model_handler: ModelHandler, fit_before_predict):
-    x_val = np.random.randn(100, len(FEATURE_NAMES))
-    dummy_model_handler.shap_file_path = Path("test_shap_file")
+# Replace FEATURE_NAMES with the actual feature names in your dataset
+FEATURE_NAMES = ["feature1", "feature2", "feature3"]
 
-    if fit_before_predict:
-        dummy_model_handler.fit()
+# Configure logger for test file
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-    if not fit_before_predict:
-        with pytest.raises(NotFittedError):
-            dummy_model_handler.calculate_shap_values(x_val)
+
+@pytest.fixture
+def dummy_shap_handler_config():
+    return ModelHandlerConfig(
+        X_train=np.random.randn(100, len(FEATURE_NAMES)),
+        y_train=np.random.randn(100),
+        fitting_mode=True,
+        file_path=None,
+        shap_file_path=None,
+        feature_names=FEATURE_NAMES,
+    )
+
+
+@pytest.fixture
+def dummy_shap_handler(dummy_shap_handler_config):
+    return ShapHandler(dummy_shap_handler_config)
+
+
+@pytest.mark.parametrize(
+    "model, should_pass",
+    [
+        (RandomForestRegressor(), True),
+        (BaseEstimator(), False),
+        (None, False),
+    ],
+)
+def test_create_explainer(dummy_shap_handler: ShapHandler, dummy_shap_handler_config: ModelHandlerConfig, model, should_pass):
+    """
+    Test creating a SHAP explainer.
+    """
+    if should_pass:
+        dummy_shap_handler.create(model.fit(
+            dummy_shap_handler_config.X_train, dummy_shap_handler_config.y_train))
+        assert isinstance(dummy_shap_handler.explainer, shap.TreeExplainer)
     else:
-        shap_values_val = dummy_model_handler.calculate_shap_values(x_val)
-        assert isinstance(shap_values_val, np.ndarray)
-        assert shap_values_val.shape == (100, len(FEATURE_NAMES))
+        with pytest.raises(ValueError):
+            dummy_shap_handler.create(model)
 
 
-@pytest.mark.parametrize("fit_before_predict, X_test, expected_exception", [
-    (False, np.random.randn(10, len(FEATURE_NAMES)), NotFittedError),
-    (True, 'invalid_input', TypeError),
-    (True, np.random.randn(10, len(FEATURE_NAMES) + 1), ValueError)
-])
-def test_calculate_shap_values_exception(dummy_model_handler: ModelHandler, fit_before_predict, X_test, expected_exception):
-    if fit_before_predict:
-        dummy_model_handler.fit()
-
-    with pytest.raises(expected_exception):
-        dummy_model_handler.calculate_shap_values(X_test)
-
-
-@pytest.mark.parametrize("model_type, valid_file", [
-    ('estimator', True),
-    ('estimator', False),
-    ('shap', True),
-    ('shap', False)
-])
-def test_save_and_load(dummy_model_handler: ModelHandler, model_type, valid_file):
-    dummy_model_handler.fitting_mode = True
-
-    if model_type == 'estimator':
-        dummy_model_handler.file_path = Path("test_estimator_file")
+@pytest.mark.parametrize("fit", [True, False])
+def test_save_and_load_explainer(dummy_shap_handler: ShapHandler, dummy_shap_handler_config: ModelHandlerConfig, fit):
+    """
+    Test saving and loading the SHAP explainer.
+    """
+    model = RandomForestRegressor()
+    if not fit:
+        with pytest.raises(ValueError):
+            dummy_shap_handler.create(model)
     else:
-        dummy_model_handler.shap_file_path = Path("test_shap_file")
+        model.fit(dummy_shap_handler_config.X_train,
+                  dummy_shap_handler_config.y_train)
+        dummy_shap_handler.create(model)
 
-    if not valid_file:
-        dummy_model_handler.file_path = 'invalid_path'
+        dummy_shap_handler_config.shap_file_path = "test_explainer.joblib"
+        dummy_shap_handler._save_explainer()
+        assert Path(dummy_shap_handler_config.shap_file_path).exists()
 
-    dummy_model_handler.fit()
+        loaded_shap_handler = ShapHandler(dummy_shap_handler_config)
+        loaded_shap_handler._load_explainer()
+        assert isinstance(loaded_shap_handler.explainer, shap.TreeExplainer)
 
-    if model_type == 'shap':
-        x_val = np.random.randn(100, len(FEATURE_NAMES))
-        dummy_model_handler.calculate_shap_values(x_val)
+        # Cleanup
+        Path(dummy_shap_handler_config.shap_file_path).unlink()
 
-    # Assert that the file was created if it's a valid file
-    if valid_file:
-        if model_type == 'estimator':
-            assert dummy_model_handler.file_path.exists()
-        else:
-            assert dummy_model_handler.shap_file_path.exists()
 
-        # Test loading from the saved file
-        dummy_model_handler_loaded = ModelHandler(
-            X_train=dummy_model_handler.X_train,
-            y_train=dummy_model_handler.y_train,
-            fitting_mode=False,
-            file_path=dummy_model_handler.file_path if model_type == 'estimator' else None,
-            shap_file_path=dummy_model_handler.shap_file_path if model_type == 'shap' else None,
-            feature_names=dummy_model_handler.feature_names,
-        )
-        dummy_model_handler_loaded.fit()
+def test_not_fitted_error(dummy_shap_handler: ShapHandler):
+    """
+    Test raising of NotFittedError when trying to access the explainer before it's created.
+    """
+    with pytest.raises(NotFittedError):
+        dummy_shap_handler.explainer
 
-        if model_type == 'shap':
-            dummy_model_handler_loaded.calculate_shap_values(x_val)
 
-        # Check if the loaded model has been fit
-        assert dummy_model_handler_loaded._estimator_handler.is_fitted == True
+def test_get_shap_data(dummy_shap_handler: ShapHandler):
+    """
+    Test the _get_shap_data function for different input sizes.
+    """
+    X_small = np.random.randn(100, len(FEATURE_NAMES))
+    X_large = np.random.randn(300, len(FEATURE_NAMES))
 
-    # Delete the file after the test if it's valid
-    if valid_file:
-        if model_type == 'estimator':
-            os.remove(dummy_model_handler.file_path)
-        else:
-            os.remove(dummy_model_handler.shap_file_path)
+    data_small = dummy_shap_handler._get_shap_data(X_small)
+    data_large = dummy_shap_handler._get_shap_data(X_large)
+
+    assert np.array_equal(data_small, X_small)
+    assert data_large.shape == (100, len(FEATURE_NAMES))
