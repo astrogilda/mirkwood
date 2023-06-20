19c548394a44500fda1b8f9edbc23fba3d2077ad
Sankalp Gilda <sankalp.gilda@gmail.com>
2023-06-20 04:09:00 -0400

    created multiple_transformer and test

--- src/multiple_transformer.py
+++ src/multiple_transformer.py
@@ -1,12 +1,10 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from utils.reshape import reshape_to_1d_array, reshape_to_2d_array
-from typing import Any, List, Optional, Union, Tuple, Dict, Callable
 from sklearn.utils.validation import check_is_fitted, check_X_y, check_array
-from sklearn.base import BaseEstimator, TransformerMixin, clone
 import numpy as np
 import logging
-from src.xandy_transformers import YTransformer, XTransformer
+from src.xandy_transformers import YTransformer
 from utils.validate import apply_transform_with_checks
+from typing import List, Optional
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,12 +15,11 @@ EPS = 1e-6
 class MultipleTransformer(BaseEstimator, TransformerMixin):
     """
     A transformer that applies a list of transformers sequentially.
-    This class is for internal use and should not be instantiated directly.
-    Please use the create_estimator function, or YTransformer, instead.
 
     Parameters
     ----------
     y_transformer : YTransformer
+    sanity_check : bool, default=True
     """
 
     def __init__(self, y_transformer: YTransformer, sanity_check: bool = True):
@@ -35,10 +32,6 @@ class MultipleTransformer(BaseEstimator, TransformerMixin):
         self.sanity_check = sanity_check
 
     def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> "MultipleTransformer":
-        if not isinstance(self.y_transformer, YTransformer):
-            raise ValueError(
-                "y_transformer should be an instance of YTransformer")
-
         if y is None:
             X = check_array(X, accept_sparse=True,
                             force_all_finite='allow-nan', ensure_2d=False)
@@ -46,39 +39,44 @@ class MultipleTransformer(BaseEstimator, TransformerMixin):
             X, y = check_X_y(X, y, accept_sparse=True,
                              force_all_finite='allow-nan', multi_output=True)
 
-        self.transformers_ = []
-        if self.y_transformer.transformers is not None:
-            for transformer in self.y_transformer.transformers:
-                # use sanity_check attribute here
-                fitted_transformer = apply_transform_with_checks(
-                    transformer=clone(transformer.transformer), method_name='fit',
-                    X=reshape_to_2d_array(X), y=y, sanity_check=self.sanity_check)
-                self.transformers_.append(fitted_transformer)
+        if hasattr(self.y_transformer, "transformers"):
+            self.transformers_ = []
+            if self.y_transformer.transformers is not None:
+                for transformer in self.y_transformer.transformers:
+                    fitted_transformer = apply_transform_with_checks(
+                        transformer=transformer.transformer, method_name='fit',
+                        X=X, y=y, sanity_check=self.sanity_check)
+                    self.transformers_.append(fitted_transformer)
+        else:
+            raise AttributeError(
+                "The input y_transformer does not have attribute 'transformers'")
         return self
 
     def transform(self, X: np.ndarray) -> np.ndarray:
         X = check_array(X, accept_sparse=True,
                         force_all_finite='allow-nan', ensure_2d=False)
-
-        if hasattr(self, "transformers_") and self.transformers_:
-            for transformer in self.transformers_:
-                # use sanity_check attribute here
-                X = apply_transform_with_checks(
-                    transformer=transformer, method_name='transform',
-                    X=reshape_to_2d_array(X), sanity_check=self.sanity_check)
-        return X
+        if hasattr(self, "transformers_"):
+            if self.transformers_:
+                for transformer in self.transformers_:
+                    X = apply_transform_with_checks(
+                        transformer=transformer, method_name='transform',
+                        X=X, sanity_check=self.sanity_check)
+            return X
+        else:
+            raise AttributeError("Transformer not fitted")
 
     def inverse_transform(self, X: np.ndarray) -> np.ndarray:
         X = check_array(X, accept_sparse=True,
                         force_all_finite='allow-nan', ensure_2d=False)
-
-        if hasattr(self, "transformers_") and self.transformers_:
-            for transformer in reversed(self.transformers_):
-                # use sanity_check attribute here
-                X = apply_transform_with_checks(
-                    transformer=transformer, method_name='inverse_transform',
-                    X=X, sanity_check=self.sanity_check)
-        return X
+        if hasattr(self, "transformers_"):
+            if self.transformers_:
+                for transformer in reversed(self.transformers_):
+                    X = apply_transform_with_checks(
+                        transformer=transformer, method_name='inverse_transform',
+                        X=X, sanity_check=self.sanity_check)
+            return X
+        else:
+            raise AttributeError("Transformer not fitted")
 
     def get_params(self, deep=True):
         return {"y_transformer": self.y_transformer, "sanity_check": self.sanity_check}
--- tests/test_multipler_transformer.py
+++ tests/test_multipler_transformer.py
@@ -4,6 +4,7 @@ from hypothesis import given, strategies as st, settings
 from sklearn.preprocessing import FunctionTransformer, StandardScaler, RobustScaler, MinMaxScaler
 from src.multiple_transformer import MultipleTransformer
 from src.xandy_transformers import XTransformer, YTransformer, TransformerConfig
+from utils.reshape import reshape_to_2d
 
 
 @st.composite
@@ -78,6 +79,7 @@ def test_multiple_transformer_wrong_input_list():
 @given(array_1d())
 def test_multiple_transformer(X):
     """Test the functionality of the MultipleTransformer class"""
+    X = reshape_to_2d(X)
     stand_scaler = StandardScaler()
     func_trans = FunctionTransformer(np.log1p)
     y_transformer = YTransformer(transformers=[
@@ -90,3 +92,70 @@ def test_multiple_transformer(X):
     assert not np.array_equal(X.ravel(), X_trans.ravel())
     X_inv = multi_trans.inverse_transform(X_trans)
     assert np.allclose(X.ravel(), X_inv.ravel(), rtol=.05)
+
+
+# Edge cases
+
+
+def test_multiple_transformer_empty_y_transformer():
+    """Test MultipleTransformer's response to an empty YTransformer"""
+    y_transformer = YTransformer(transformers=[])
+    multi_trans = MultipleTransformer(y_transformer=y_transformer)
+    # Expecting no exceptions here as the transformer list is empty but valid
+    multi_trans.fit(np.array([[1, 2], [3, 4]]))
+    # Check that the transformation is a passthrough
+    X = np.array([5, 6, 7, 8])
+    X_trans = multi_trans.transform(X)
+    assert np.array_equal(X, X_trans)
+
+
+def test_multiple_transformer_y_transformer_with_invalid_transformer():
+    """Test MultipleTransformer's response to a YTransformer with an invalid transformer"""
+    from pydantic import ValidationError
+    with pytest.raises(ValidationError):
+        y_transformer = YTransformer(transformers=[None])
+
+
+def test_multiple_transformer_y_transformer_without_transformer_attribute():
+    """Test MultipleTransformer's response to a YTransformer without transformer attribute"""
+    with pytest.raises(AttributeError):
+        y_transformer = YTransformer()
+        del y_transformer.transformers
+        multi_trans = MultipleTransformer(y_transformer=y_transformer)
+        multi_trans.fit(np.array([[1, 2], [3, 4]]))
+
+
+def test_multiple_transformer_multiple_transformers():
+    """Test the functionality of the MultipleTransformer class with multiple transformers"""
+    transformers_list = [
+        TransformerConfig(name="standard_scaler",
+                          transformer=StandardScaler()),
+        TransformerConfig(name="min_max_scaler", transformer=MinMaxScaler()),
+        TransformerConfig(name="robust_scaler", transformer=RobustScaler())
+    ]
+    y_transformer = YTransformer(transformers=transformers_list)
+    multi_trans = MultipleTransformer(y_transformer=y_transformer)
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    multi_trans.fit(X)
+    X_trans = multi_trans.transform(X)
+    assert not np.array_equal(X, X_trans)
+    X_inv = multi_trans.inverse_transform(X_trans)
+    assert np.allclose(X, X_inv, rtol=.05)
+
+
+@settings(deadline=None)
+@given(array_2d())
+def test_multiple_transformer_with_2d_input(X):
+    """Test the functionality of the MultipleTransformer class with 2D input"""
+    transformers_list = [
+        TransformerConfig(name="standard_scaler",
+                          transformer=StandardScaler()),
+        TransformerConfig(name="min_max_scaler", transformer=MinMaxScaler()),
+    ]
+    y_transformer = YTransformer(transformers=transformers_list)
+    multi_trans = MultipleTransformer(y_transformer=y_transformer)
+    multi_trans.fit(X)
+    X_trans = multi_trans.transform(X)
+    assert not np.array_equal(X, X_trans)
+    X_inv = multi_trans.inverse_transform(X_trans)
+    assert np.allclose(X, X_inv, rtol=.05)
