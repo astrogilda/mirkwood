91a188799ee25b38b1596e7167939a8db9e43f2d
Sankalp Gilda <sankalp.gilda@gmail.com>
2023-06-21 08:24:49 -0400

    refactoring test_postprocessy_transformer

--- src/transformers/postprocessy_transformer.py
+++ src/transformers/postprocessy_transformer.py
@@ -1,50 +1,85 @@
-from sklearn.utils.validation import check_is_fitted
 from sklearn.base import BaseEstimator, TransformerMixin
-from typing import Optional, Callable, Dict, Union, Tuple
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_array, check_is_fitted
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from handlers.data_handler import GalaxyProperty
-from typing import Any, List, Optional, Union, Tuple, Dict, Callable
-from sklearn.utils.validation import check_is_fitted, check_X_y, check_array
-from sklearn.base import BaseEstimator, TransformerMixin, clone
 import numpy as np
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-EPS = 1e-6
-
 
 class PostProcessY(BaseEstimator, TransformerMixin):
     """
-    Custom transformer to postprocess data according to the specified galaxy property.
+    Transformer for applying postprocessing steps on output data based on specified galaxy properties.
 
-    Parameters
+    Attributes
     ----------
-    prop : GalaxyProperty
-        The galaxy property to apply the inverse transform.
+    prop : str
+        The name of the galaxy property to be used for postprocessing.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Checks the input data and sets up the transformer.
+    transform(X)
+        Applies the postprocessing steps on the data.
+    _apply_inverse_transform(X, inverse_transforms)
+        Applies the specific inverse transformation based on the galaxy property.
     """
 
-    def __init__(self, prop: Optional[GalaxyProperty] = None):
+    def __init__(self, prop: Optional[str] = None):
+        """
+        Constructs all the necessary attributes for the PostProcessY object.
+
+        Parameters
+        ----------
+        prop : str
+            The name of the galaxy property to be used for postprocessing.
+        """
         self.prop = prop
 
     @staticmethod
     def _get_label_rev_func() -> Dict[str, Callable[[np.ndarray], np.ndarray]]:
-        """Get inverse transforms for galaxy properties."""
-        return {GalaxyProperty.STELLAR_MASS: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=20)),
-                GalaxyProperty.DUST_MASS: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=20)) - 1,
-                GalaxyProperty.METALLICITY: lambda x: np.float_power(10, np.clip(x, a_min=-1e1, a_max=1e1)),
-                GalaxyProperty.SFR: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=1e2)) - 1,
-                }
+        """
+        Static method that returns a dictionary of lambda functions for the inverse transformations
+        of the different galaxy properties.
+
+        Returns
+        -------
+        Dict[str, Callable[[np.ndarray], np.ndarray]]
+            A dictionary with keys as galaxy properties and values as the inverse transformation functions.
+        """
+        return {
+            GalaxyProperty.STELLAR_MASS: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=20)),
+            GalaxyProperty.DUST_MASS: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=20)) - 1,
+            GalaxyProperty.METALLICITY: lambda x: np.float_power(10, np.clip(x, a_min=-1e1, a_max=1e1)),
+            GalaxyProperty.SFR: lambda x: np.float_power(10, np.clip(x, a_min=0, a_max=1e2)) - 1,
+        }
 
     def fit(self, X: np.ndarray, y=None) -> 'PostProcessY':
         """
-        Fit the transformer. 
-        Not used in this transformer, hence only checks for complex data and returns self.
-        """
+        Checks the input data and sets up the transformer.
 
+        Parameters
+        ----------
+        X : np.ndarray
+            Input data.
+        y : np.ndarray, optional
+            Target data. (Default is None)
+
+        Raises
+        ------
+        ValueError
+            If X is complex or non-numeric data.
+
+        Returns
+        -------
+        self : PostProcessY
+            The instance of the transformer.
+        """
         X = check_array(X, accept_sparse=True,
-                        force_all_finite=False, ensure_2d=False, dtype=None)
+                        force_all_finite=False, ensure_2d=False)
 
         if np.iscomplexobj(X):
             raise ValueError("Complex data not supported")
@@ -52,52 +87,64 @@ class PostProcessY(BaseEstimator, TransformerMixin):
         if X.dtype == object:
             raise ValueError("Non-numeric data (dtype=object) not supported")
 
-        # Check the prop value is valid
         inverse_transforms = self._get_label_rev_func()
-        if self.prop is not None and self.prop.value not in inverse_transforms.keys():
-            raise ValueError("Invalid prop value")
+        if self.prop is not None and self.prop not in GalaxyProperty._member_names_:
+            raise ValueError(f"Invalid prop value: '{self.prop}'")
 
+        self.prop = GalaxyProperty(self.prop) if self.prop else None
+        self.is_fitted_ = True
         return self
 
-    def transform(self, *Xs: Union[np.ndarray, Tuple[np.ndarray]]) -> Union[np.ndarray, Tuple[np.ndarray]]:
+    def transform(self, X: np.ndarray) -> np.ndarray:
         """
-        Apply inverse transformation to the data according to the specified galaxy property.
+        Applies the postprocessing steps on the data.
 
         Parameters
         ----------
-        *ys : Union[np.ndarray, Tuple[np.ndarray]]
-            Preprocessed data.
+        X : np.ndarray
+            Input data.
+
+        Raises
+        ------
+        ValueError
+            If X is complex or non-numeric data.
 
         Returns
         -------
-        Union[np.ndarray, Tuple[np.ndarray]]
+        np.ndarray
             Postprocessed data.
         """
-        # Ensure fit has been called
         check_is_fitted(self)
 
-        if any(np.iscomplexobj(X) for X in Xs):
+        if np.iscomplexobj(X):
             raise ValueError("Complex data not supported")
 
-        for X in Xs:
-            if X.dtype == object:
-                raise ValueError(
-                    "Non-numeric data (dtype=object) not supported")
-            X = check_array(X, accept_sparse=True,
-                            force_all_finite=False, ensure_2d=False, dtype=None)
+        if X.dtype == object:
+            raise ValueError("Non-numeric data (dtype=object) not supported")
 
+        X = check_array(X, accept_sparse=True,
+                        force_all_finite=False, ensure_2d=False)
         inverse_transforms = self._get_label_rev_func()
+
         if self.prop is None:
-            return Xs
+            return X
         else:
-            postprocessed_Xs = [self._apply_inverse_transform(
-                X, inverse_transforms) for X in Xs]
-            return tuple(postprocessed_Xs) if len(postprocessed_Xs) > 1 else postprocessed_Xs[0]
+            return self._apply_inverse_transform(X, inverse_transforms)
 
     def _apply_inverse_transform(self, X: np.ndarray, inverse_transforms: Dict[str, Callable]) -> np.ndarray:
-        """Apply the inverse transformation for the specific galaxy property."""
-        postprocessed_X = np.zeros_like(X)
-        for key, func in inverse_transforms.items():
-            if self.prop.value in key:
-                postprocessed_X = func(X)
-        return postprocessed_X
+        """
+        Applies the specific inverse transformation based on the galaxy property.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            Input data.
+        inverse_transforms : Dict[str, Callable]
+            A dictionary of the inverse transformation functions for each galaxy property.
+
+        Returns
+        -------
+        np.ndarray
+            Postprocessed data.
+        """
+        return inverse_transforms[self.prop.value](X) if self.prop in inverse_transforms else X
--- tests/src/transformers/test_postprocessy_transformer.py
+++ tests/src/transformers/test_postprocessy_transformer.py
@@ -1,53 +1,105 @@
-from handlers.data_handler import DataHandler, DataHandlerConfig, GalaxyProperty
-import numpy as np
+import pytest
+from handlers.data_handler import GalaxyProperty
 from transformers.postprocessy_transformer import PostProcessY
+import numpy as np
 
 
-def test_postprocessy():
+def test_postprocessy_stellar_mass():
     """
-    Test to check if the DataHandler postprocess_y method works correctly.
+    Test to check if the PostProcessY works correctly for STELLAR_MASS.
     """
-    dtype = np.dtype([
-        ('log_stellar_mass', float),
-        ('log_dust_mass', float),
-        ('log_metallicity', float),
-        ('log_sfr', float),
-    ])
-    y_array = np.zeros(2, dtype=dtype)
-    y_array['log_stellar_mass'] = [1, 2]
-    y_array['log_dust_mass'] = [3, 4]
-    y_array['log_metallicity'] = [5, 6]
-    y_array['log_sfr'] = [7, 8]
-
-    config = DataHandlerConfig(mulfac=1.0)
-    handler = DataHandler(config)
-    postprocessed_y = PostProcessY(prop=GalaxyProperty.STELLAR_MASS).transform(
-        y_array['log_stellar_mass'])
-    expected_output = np.zeros(2, dtype=float)
-    expected_output = [10, 100]
+    log_stellar_mass = np.array([1, 2])
+    transformer = PostProcessY(prop="stellar_mass")
+    transformer.fit(log_stellar_mass)
+    postprocessed_y = transformer.transform(log_stellar_mass)
+    expected_output = np.array([10, 100])
     np.testing.assert_array_equal(postprocessed_y, expected_output)
 
-    postprocessed_y = PostProcessY(
-        prop=GalaxyProperty.DUST_MASS).transform(y_array['log_dust_mass'])
-    expected_output = np.zeros(2, dtype=float)
-    expected_output = [999, 9999]
+
+def test_postprocessy_dust_mass():
+    """
+    Test to check if the PostProcessY works correctly for DUST_MASS.
+    """
+    log_dust_mass = np.array([3, 4])
+    transformer = PostProcessY(prop="dust_mass")
+    transformer.fit(log_dust_mass)
+    postprocessed_y = transformer.transform(log_dust_mass)
+    expected_output = np.array([999, 9999])
     np.testing.assert_array_equal(postprocessed_y, expected_output)
 
-    postprocessed_y = PostProcessY(
-        prop=GalaxyProperty.METALLICITY).transform(y_array['log_metallicity'])
-    expected_output = np.zeros(2, dtype=float)
-    expected_output = [100000, 1000000]
+
+def test_postprocessy_metallicity():
+    """
+    Test to check if the PostProcessY works correctly for METALLICITY.
+    """
+    log_metallicity = np.array([5, 6])
+    transformer = PostProcessY(prop="metallicity")
+    transformer.fit(log_metallicity)
+    postprocessed_y = transformer.transform(log_metallicity)
+    expected_output = np.array([100000, 1000000])
     np.testing.assert_array_equal(postprocessed_y, expected_output)
 
-    postprocessed_y = PostProcessY(
-        prop=GalaxyProperty.SFR).transform(y_array['log_sfr'])
-    expected_output = np.zeros(2, dtype=float)
-    expected_output = [9999999, 99999999]
+
+def test_postprocessy_sfr():
+    """
+    Test to check if the PostProcessY works correctly for SFR.
+    """
+    log_sfr = np.array([7, 8])
+    transformer = PostProcessY(prop="sfr")
+    transformer.fit(log_sfr)
+    postprocessed_y = transformer.transform(log_sfr)
+    expected_output = np.array([9999999, 99999999])
     np.testing.assert_array_equal(postprocessed_y, expected_output)
 
-    # test if postprocess_y raises an error when ys is a tuple of arrays
-    postprocessed_y = PostProcessY(prop=GalaxyProperty.SFR).transform(
-        (y_array['log_sfr'], y_array['log_sfr']))
-    expected_output = np.zeros(2, dtype=float)
-    expected_output = [[9999999, 99999999], [9999999, 99999999]]
-    np.testing.assert_array_equal(postprocessed_y, expected_output)
+
+def test_postprocessy_invalid_prop():
+    """
+    Test to check if the PostProcessY raises an error for an invalid prop.
+    """
+    log_sfr = np.array([7, 8])
+    transformer = PostProcessY(prop='INVALID')
+    with pytest.raises(ValueError):
+        transformer.fit(log_sfr)
+
+
+def test_postprocessy_complex_data():
+    """
+    Test to check if the PostProcessY raises an error for complex data.
+    """
+    complex_data = np.array([1+1j, 2+2j])
+    transformer = PostProcessY(prop="sfr")
+    with pytest.raises(ValueError):
+        transformer.fit(complex_data)
+
+
+def test_postprocessy_non_numeric_data():
+    """
+    Test to check if the PostProcessY raises an error for non-numeric data.
+    """
+    non_numeric_data = np.array(['a', 'b'])
+    transformer = PostProcessY(prop="sfr")
+    with pytest.raises(ValueError):
+        transformer.fit(non_numeric_data)
+
+
+def test_postprocessy_transform_without_fit():
+    """
+    Test to check if the PostProcessY raises an error when transform is called without first calling fit.
+    """
+    log_sfr = np.array([7, 8])
+    transformer = PostProcessY(prop="sfr")
+    with pytest.raises(Exception):
+        transformer.transform(log_sfr)
+
+
+def test_postprocessy_inverse_transform_output():
+    """
+    Test to check if the output of inverse_transform is almost equal to the input.
+    """
+    log_sfr = np.array([7, 8])
+    transformer = PostProcessY(prop="sfr")
+    transformer.fit(log_sfr)
+    postprocessed_y = transformer.transform(log_sfr)
+    inverse_transformed_y = transformer._apply_inverse_transform(
+        postprocessed_y, transformer._get_label_rev_func())
+    np.testing.assert_array_almost_equal(inverse_transformed_y, log_sfr)
